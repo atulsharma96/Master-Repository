14883f1fe58b8575cb1399d61abde2e583d3b059
Atul Sharma <atulsharmaoffice@gmail.com>
2018-10-04 14:08:23 -0600

    Removed incomplete project ID3.

--- ID3DecisionTree/README.txt
+++
@@ -1,3 +0,0 @@
-simply execute the shell file as:
-"sh run.sh"
-with the appropriate permissions
--- ID3DecisionTree/run.sh
+++
@@ -1,2 +0,0 @@
-python3 tester.py && python3 cross_validation_runner.py
-# The code takes about 5-6 minutes to run in all.
